c5adaff54f00a79c2d7f10b8a949696294a9bc11
Dmitrii Koshelev <me@koshelev.net>
2020-12-24 17:09:34 +0300

    Fixes after 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
 let editButton = document.querySelector('.button_type_edit');
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.button_type_close');
-let nameField = popup.querySelector('.popup__name');
+let nameField = popup.querySelector('.popup__input_type_text[name=input-name]');
 let userName = document.querySelector('.user-info__name');
-let descriptionField = popup.querySelector('.popup__description');
+let descriptionField = popup.querySelector('.popup__input_type_text[name=input-description]');
 let userDescription = document.querySelector('.user-info__description');
 let popupForm = popup.querySelector('.popup__form');
 
 
-function togglePopup () {
+function togglePopup() {
   popup.classList.toggle('popup_opened');
 }
 
@@ -19,18 +19,23 @@ function handleFormSubmit (event) {
   togglePopup();
 }
 
-editButton.addEventListener('click', () => {
+function openEditForm() {
   nameField.value = userName.textContent;
   descriptionField.value = userDescription.textContent;
   togglePopup();
-});
+}
+
+function closePopup(event) {
+  if (event.target === event.currentTarget) {
+    togglePopup();
+  }
+}
+
+
+editButton.addEventListener('click', openEditForm);
 
 closeButton.addEventListener('click', togglePopup);
 
 popupForm.addEventListener('submit', handleFormSubmit); 
 
-popup.addEventListener('click', (event) => {
-  if (event.target === event.currentTarget) {
-    togglePopup();
-  }
-})
+popup.addEventListener('click', closePopup);
